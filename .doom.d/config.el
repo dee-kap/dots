25d4ed925e3bd5d8457cb6e14c3c202839bc55eb
Deepak Kapoor <kapoordeepak@gmail.com>
2020-12-03 18:48:10 +1100

    update dots

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -19,9 +19,10 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Source Code Pro" :size 15 :weight 'normal)
-      doom-variable-pitch-font (font-spec :family "Ubuntu" :size 15)
-      doom-big-font (font-spec :family "Source Code Pro" :size: 24))
+;; (setq doom-font (font-spec :family "Fira Code" :size 15 :weight 'normal)
+;;       doom-big-font (font-spec :family "Fira Code" :size: 22))
+(setq doom-font (font-spec :family "Fira Code" :size 15 )
+      doom-big-font (font-spec :family "Fira Code" :size: 22))
 (after! doom-themes
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
@@ -29,7 +30,7 @@
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
 
-(setq-default line-spacing 4)
+(setq-default line-spacing 6)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
