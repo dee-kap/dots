9bfb50d08c92228130ccd5db15a17a2eaccebf5e
Deepak Kapoor <kapoordeepak@gmail.com>
2020-12-05 09:15:25 +1100

    Add lsp-python-ms

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -85,7 +85,7 @@
 
 (setq company-dabbrev-downcase 0)
 (setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
+(setq company-minimum-prefix-length 0)
 (setq company-selection-wrap-around t)
 
 (setq treemacs-git-mode 'extended)
---
+++ .doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic)))))
